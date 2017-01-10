f755c75182359e4ea055c810239e897179dc356e
geoHeil <georg.kf.heiler@gmail.com>
2017-01-10 12:35:02 +0100

    wordcloud re-added

--- tweets2.R
+++ tweets2.R
@@ -29,6 +29,24 @@ ggplot(tweet_picture_counts, aes(source, n, fill = picture)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(x = "", y = "Number of tweets", fill = "")
 
+tweetsSmall %>%
+  count(source,
+        quoted = ifelse(str_detect(text, '^"'), "Quoted", "Not quoted")) %>%
+  ggplot(aes(source, n, fill = quoted)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "", y = "Number of tweets", fill = "") +
+  ggtitle('Whether tweets start with a quotation mark (")')
+
+
+tweet_words %>%
+  count(word, sort = TRUE) %>%
+  head(20) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_bar(stat = "identity") +
+  ylab("Occurrences") +
+  coord_flip()
+
 library(tidytext)
 reg <- "([^A-Za-z\\d#@']|'(?![A-Za-z\\d#@]))"
 tweet_words <- tweetsSmall %>%
@@ -39,3 +57,32 @@ tweet_words <- tweetsSmall %>%
          str_detect(word, "[a-z]"))
 
 tweet_words
+
+android_iphone_ratios <- tweet_words %>%
+  count(word, source) %>%
+  filter(sum(n) >= 5) %>%
+  spread(source, n, fill = 0) %>%
+  ungroup() %>%
+  mutate_each(funs((. + 1) / sum(. + 1)), -word) %>%
+  mutate(logratio = log2(Android / iPhone)) %>%
+  arrange(desc(logratio))
+
+
+########################
+wordCorpus <- Corpus(VectorSource(tweet_words))
+tdm <- TermDocumentMatrix(wordCorpus,
+                          control = list(wordLengths = c(1, Inf)))
+term.freq <- rowSums(as.matrix(tdm))
+term.freq <- subset(term.freq, term.freq >= 20)
+df <- data.frame(term = names(term.freq), freq = term.freq)
+ggplot(df, aes(x=term, y=freq)) +
+  geom_bar(stat="identity") +
+  xlab("Terms") +
+  ylab("Count") +
+  coord_flip() 
+
+m <- as.matrix(tdm)
+word.freq <- sort(rowSums(m), decreasing = T)
+pal <- brewer.pal(9, "BuGn")[-(1:4)]
+
+wordcloud(words = names(word.freq), freq = word.freq, min.freq = 3, random.order = F, colors = pal)
