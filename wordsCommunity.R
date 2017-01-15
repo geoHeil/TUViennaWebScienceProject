4c6a3168e3638c86258b0836a7da8f80e1c69a7b
geoHeil <georg.kf.heiler@gmail.com>
2017-01-15 13:14:32 +0100

    how to access tweets

--- wordsCommunity.R
+++ wordsCommunity.R
@@ -1,5 +1,5 @@
 # words for communities
-wants <- c("SocialMediaLab", "magrittr", "igraph", "ggplot2", "wordcloud", "tm", "stringr", "lubridate", "scales", "dplyr", "purrr", "tidyr","twitteR","tm","wordcloud","readr","htmlwidgets","streamgraph")
+wants <- c("SocialMediaLab", "magrittr", "igraph", "ggplot2", "wordcloud", "tm", "stringr", "lubridate", "scales", "dplyr", "purrr", "tidyr","twitteR","tm","wordcloud","readr","htmlwidgets")
 has   <- wants %in% rownames(installed.packages())
 if(any(!has)) install.packages(wants[!has])
 lapply(wants, library, character.only=T)
@@ -13,7 +13,54 @@ c$no
 summary(c$csize)
 sort(c$csize, decreasing = T)[1:10]
 
+makeWordcloud <- function(t){
+  nohandles <- str_replace_all(t$text, "@\\w+", "") 
+  wordCorpus <- Corpus(VectorSource(nohandles))
+  wordCorpus <- tm_map(wordCorpus,
+                       content_transformer(function(x) iconv(x,
+                                                             to='UTF-8',
+                                                             sub='byte')),
+                       mc.cores=1)
+  wordCorpus <- tm_map(wordCorpus,
+                       removePunctuation,
+                       lazy=TRUE)
+  wordCorpus <- tm_map(wordCorpus,
+                       content_transformer(tolower),
+                       lazy=TRUE)
+  wordCorpus <- tm_map(wordCorpus,
+                       removeWords,
+                       stopwords("english"),
+                       lazy=TRUE)
+  wordCorpus <- tm_map(wordCorpus,
+                       removeNumbers,
+                       lazy=TRUE)
+  wordCorpus <- tm_map(wordCorpus,
+                       stripWhitespace,
+                       lazy=TRUE)
+  wordCorpus <- tm_map(wordCorpus,
+                       removeWords,
+                       c("trump","realdonaldtrump","the",
+                         "like","http","https","httpst"))
+  pal <- brewer.pal(9,"Reds")
+  pal <- pal[-(1:4)]
+  set.seed(123)
+  wordcloud(words = wordCorpus,
+            scale=c(6,0.1),
+            max.words=250,
+            random.order=FALSE, 
+            rot.per=0.35,
+            use.r.layout=FALSE,
+            colors=pal)
+}
+makeWordcloud(tweets)
 # this creates a sub cluster
 # TODO iterate over the top 3 clusters (or coose any other)
 # and just execute the wordcount code
 subCluster <- induced.subgraph(g_twitter_actor, V(g_twitter_actor)[which(c$membership == which.max(c$csize))])
+
+# TODO how to access tweets / text object wihtin cluster
+
+for(index in 0:2) {
+  print(index)
+  # TODO use subcluster here
+}
